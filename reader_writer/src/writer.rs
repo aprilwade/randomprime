417a12536c398cf97e3c3cd379553b6d04b3ce51
Tyler Wade <wayedt@gmail.com>
2017-05-02 03:13:23 -0500

    Move web components to gh-pages

--- reader_writer/src/writer.rs
+++
@@ -1,6 +0,0 @@
-use std::io::{Result, Write};
-
-pub trait Writable
-{
-    fn write<W: Write>(&self, writer: &mut W) -> Result<()>;
-}
--- structs/src/lib.rs
+++
@@ -1,9 +0,0 @@
-include!(concat!(env!("OUT_DIR"), "/lib.rs"));
-
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
