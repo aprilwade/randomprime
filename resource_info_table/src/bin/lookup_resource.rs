849175a7ea234668c2a9752edb3db998e043046d
April Wade <wayedt@gmail.com>
2019-07-07 16:31:31 -0500

    Refactor resource_info_table

--- resource_info_table/src/bin/lookup_resource.rs
+++ resource_info_table/src/bin/lookup_resource.rs
@@ -2,19 +2,21 @@ use resource_info_table::{
     resource_info, ResourceInfo
 };
 
+use macro_file_proxy::macro_file_proxy_item;
+
 use std::env::args;
 use std::u32;
 
 
 macro_rules! build_resource_info_array {
-    ($(($long_name:tt, $res_id:expr, $fourcc:expr, $paks:expr, $($short_name:tt)?))+) => {
+    ($($long_name:tt, $res_id:expr, $fourcc:expr, $paks:expr $(, $short_name:tt)?;)+) => {
         const RESOURCE_INFO: &[$crate::ResourceInfo] = &[
             $(resource_info!($long_name),)+
         ];
     };
 }
 
-resource_info_table::forward_resource_info!(build_resource_info_array);
+macro_file_proxy_item! { "resource_info.txt", build_resource_info_array, ;}
 
 pub fn lookup_resource_info(res_id: u32) -> Option<ResourceInfo>
 {
